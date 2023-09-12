ca0f97f051a1746dd978c3cedbf4aac4859a13c5
Nathan Pedigo <njpedigo@gmail.com>
2023-09-12 09:13:14 -0600

    imported firebase and created forms from the tutorial

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAYYLv3cuK4p1hkx3xkh67_Wfg2ZhjRZ-4",
+    authDomain: "angular-firebase-example-70a8c.firebaseapp.com",
+    projectId: "angular-firebase-example-70a8c",
+    storageBucket: "angular-firebase-example-70a8c.appspot.com",
+    messagingSenderId: "492197946838",
+    appId: "1:492197946838:web:07bd108dad3bb607637077",
+    measurementId: "G-0WMNLK00T4"
+  }
+};
